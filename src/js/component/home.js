c3a9289b255e6c7dd80388deafe878dd5043fc89
jmar76 <jmaguero.ing@gmail.com>
2021-05-04 15:20:06 +0000

    ejercicio terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,79 @@
 import React from "react";
+import "../../styles/home.scss";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	let numero = [];
+	const generatorRandomNumber = () => {
+		let numbers = [
+			"A",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"J",
+			"Q",
+			"K"
+		];
+
+		let indexNumbers = Math.floor(Math.random() * numbers.length);
+		return numero.push(numbers[indexNumbers]);
+	};
+	generatorRandomNumber();
+	console.log(numero);
+
+	let simbolo = [];
+	const generatorRandomSuit = () => {
+		let suit = ["diamond", "spade", "heart", "club"];
+		let indexSuit = Math.floor(Math.random() * suit.length);
+
+		return simbolo.push(suit[indexSuit]);
+	};
+	generatorRandomSuit();
+	console.log(simbolo);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid text-center">
+			<div className=""></div>
+			{simbolo == "spade" ? (
+				<div className="card spade">
+					{" "}
+					<span className="number">{numero}</span>
+				</div>
+			) : (
+				""
+			)}
+
+			{simbolo == "diamond" ? (
+				<div className="card diamond">
+					{" "}
+					<span className="number">{numero}</span>
+				</div>
+			) : (
+				""
+			)}
+
+			{simbolo == "club" ? (
+				<div className="card club">
+					{" "}
+					<span className="number">{numero}</span>
+				</div>
+			) : (
+				""
+			)}
+
+			{simbolo == "heart" ? (
+				<div className="card heart">
+					{" "}
+					<span className="number">{numero}</span>
+				</div>
+			) : (
+				""
+			)}
 		</div>
 	);
 }
